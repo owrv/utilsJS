46f7bc7f14628724a6c30c6613888b3a18487286
Wellersom Richardsom Voigt <wellersom.wrv@gmail.com>
2022-10-29 18:58:25 -0300

    parameter correction

--- replic.js
+++ replic.js
@@ -44,7 +44,7 @@ console.log(result)
 
 function exempleTwo() {
 const parameterOne = ['RCSL4', 'CIEL3', 'POMO4', 'GFSA3', 'TPIS3', 'COGN3', 'LPSB3', 'TECN3', 'MEAL3', 'PLPL3', 'HBSA3', 'SAPR4', 'BMGB4', 'OPCT3', 'ENJU3', 'LAME3', 'NGRD3', 'IRBR3', 'LAME4', 'RCSL3', 'SHOW3', 'TCSA3', 'AMAR3']
-const parameterTwo = ["1,09", "5,64", "2,76", "7,66", "1,66", "3,18", "2,8", "2,13", "2,34", "4,17", "2,6", "3,85", "2,5", "2,55", "1,16", "#N/A", "1,53", "0,93", "#N/A", "2,04", "2,83", "3,84", "1,99"]
+    const parameterTwo = ["1.09", "5.64", "2.76", "7.66", "1.66", "3.18", "2.8", "2.13", "2.34", "4.17", "2.6", "3.85", "2.5", "2.55", "1.16", "#N/A", "1.53", "0.93", "#N/A", "2.04", "2.83", "3.84", "1.99"]
 const query = "insert into smallCaps (code, price) values\n"
 
 let unity = []
