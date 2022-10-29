4c7d729101b42bd40013a37ffaf2c54129e8b4da
Wellersom Richardsom Voigt <wellersom.wrv@gmail.com>
2022-10-29 18:54:56 -0300

    Created function to demonstrate examples

--- replic.js
+++ replic.js
@@ -29,18 +29,20 @@ function replicate(argOne, argTwo) {
     Exemplos de uso: 
         incluir uma nova condicional 
 
-const obj = ["Lol", "hearthstone"]
+function exempleOne() {
 const query = "delete from games where des_name in"
 
 const result = replicate(query, obj)
 
 console.log(result)
 
-*/
+}
 
 /* 
 *   Também é possível passa fazer desta maneira: 
 */
+
+function exempleTwo() {
 const parameterOne = ['RCSL4', 'CIEL3', 'POMO4', 'GFSA3', 'TPIS3', 'COGN3', 'LPSB3', 'TECN3', 'MEAL3', 'PLPL3', 'HBSA3', 'SAPR4', 'BMGB4', 'OPCT3', 'ENJU3', 'LAME3', 'NGRD3', 'IRBR3', 'LAME4', 'RCSL3', 'SHOW3', 'TCSA3', 'AMAR3']
 const parameterTwo = ["1,09", "5,64", "2,76", "7,66", "1,66", "3,18", "2,8", "2,13", "2,34", "4,17", "2,6", "3,85", "2,5", "2,55", "1,16", "#N/A", "1,53", "0,93", "#N/A", "2,04", "2,83", "3,84", "1,99"]
 const query = "insert into smallCaps (code, price) values\n"
@@ -63,3 +65,5 @@ parameterOne.forEach((one, indexOne) => {
 
 
 console.log(query + result)
+}
+
